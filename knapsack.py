e85bf45c7bb2620a178e2df3504ab2d0c66b9ac6
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-03-07 17:08:03 +0000

    Add files via upload

--- knapsack.py
+++ knapsack.py
@@ -1,56 +1,28 @@
+import numpy
 from collections import namedtuple
-import csv
-import os
-Item = namedtuple("Item", ['index', 'value', 'weight'])
 
-# El método te devuelve la lista de items rellena, item_count y capacity
 def insertItems():
+    # Este es el ejemplo del fichero ks_4
+    Item = namedtuple("Item", ['index', 'value', 'weight'])
     items = []
-    item_count = 0
-    capacity = 0
     bound = 0
-
-    # Imprime el/los ficheros de la carpeta
-    for dirname, _, filenames in os.walk('data'):
-        for filename in filenames:
-            print(os.path.join(dirname, filename))
-    
-    # Abre el primer fichero (debe haber una carpeta llamada "data" en el cual puede poner el fichero que quiere leer)
-    # la carpeta data debe estar en el mismo directorio en el cual se encuentra este proyecto 
-    for dirname, _, filenames in os.walk('data'):
-        for filename in filenames:
-            full_name = dirname+'/'+filename
-            with open(full_name, 'r') as input_data_file:
-                input_data = input_data_file.read()
-
-
-                lines = input_data.split('\n')
-
-                firstLine = lines[0].split()
-                item_count = int(firstLine[0])
-                capacity = int(firstLine[1])
-                
-                j = 1
-                for i in range(1, item_count+1): 
-                    line = lines[i]
-                    parts = line.split()
-                    if int(parts[1]) <= capacity:            
-                        items.append(Item(j-1,int(parts[0]), int(parts[1])))
-                        bound += int(parts[0])
-                        j = j+1
-                item_count = len(items)
-                break
-    return items, item_count, capacity
-
+    items.append(Item(0, 8, 4))
+    items.append(Item(1, 10, 5))
+    items.append(Item(2, 15, 8))
+    items.append(Item(3, 4, 3))
+    capacity = 11
+    for i in items:
+        bound += i.value
+    return items, len(items), capacity, bound
 
 class Node:
 
-    def __init__(self, root, eValue, eWeight, bound):
-        self.father = root
-        self.right = None
+    def __init__(self, root, value, capacity, bound):
+        self.root = root
         self.left = None
-        self.value = eValue
-        self.weight = eWeight
+        self.right = None
+        self.value = value
+        self.room = capacity
         self.estimate = bound
 
     def insert(self, newValue, newRoom, newEstimate):
@@ -60,57 +32,39 @@ class Node:
         elif newNode.estimate == self.estimate:
             self.left = newNode
 
-    def printTree(self):
+# Print tree
+    def PrintTree(self):
         if self.left:
-            self.left.printTree()
-        print([self.value, self.weight, self.estimate]),
+            self.left.PrintTree()
+        print([self.value, self.room, self.estimate]),
         if self.right:
-            self.right.printTree()
+            self.right.PrintTree()
+
+def greedy(items, capacity, estimate):
+    total = 0
+    taken = []
+    for item in items: 
+        if capacity - item.weight >= 0:
+            total += item.value
+            capacity -= item.weight
+            taken.append(1)
+        else:
+            estimate -= item.value
+            taken.append(0)
+    return total, capacity, estimate, taken
 
 if __name__ == "__main__":
-    items, item_count, capacity = insertItems()
-    taken = [0]*len(items)
-    estimate = 0
-    for i in items:
-        estimate += i.value
-    
-    root = Node(None, 0, capacity, estimate)
-    act = root
-    i = 0
-    result = Node(None, 0, 0, 0)
-    
-    while root.left is None or root.right is None or act.father is not None:
-        if act.left is None and act.weight - items[i].weight >= 0:
-            act.insert(act.value + items[i].value, act.weight - items[i].weight, act.estimate)
-            ant = act
-            act = act.left
-        else:
-            act.insert(act.value, act.weight, act.estimate - items[i].value)
-            ant = act
-            act = act.right
-        i += 1
+    items, item_count, capacity, bound = insertItems()
 
-        if act.estimate < result.estimate or i == len(items):
-            if act.estimate > result.estimate:
-                result = act
-            i -= 1
-            while ant.right is not None and ant.father is not None:
-                act = ant
-                ant = act.father
-                i -= 1
-            act = ant
-            ant = act.father
+    # La primera busqueda sera greedy para llegar un nodo hoja
+    greedyValue, room, estimate, takenGreedy = greedy(items, capacity, bound)
+    result = Node(None, greedyValue, room, estimate)
+    print("result: ", "value: " + str(result.value), "room: " + str(result.room), "estimate: " + str(result.estimate), sep=" --- ")
+    print("taken of result", takenGreedy)
 
-    #print(root.printTree())
-
-    i = len(items) - 1
-    aux = result.father
-    while(aux != None):
-        if aux.value != aux.value:
-            taken[i] = 1
-        else:
-            taken[i] = 0
-        result = result.father
-        aux = aux.father
-        i -= 1
-    print("taken vector: ", taken)
+    # Empezar a expandir el arbol comparando con el estimate de result
+    ## Asi habra algunas veces que ni tendra que ir hasta el nodo hoja
+    ### Con la lista abierta primero meter un nodo (None, 0, 11, 37)
+    """for item in items:
+        check = O.pop()
+        if item.value + check.value > result.estimate:"""
