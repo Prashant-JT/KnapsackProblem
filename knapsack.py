524874dadfcd52c0fe044d136514ec84d61105ae
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-04-10 06:02:41 +0100

    implemented greedy and branch and bound

--- knapsack.py
+++ knapsack.py
@@ -1,112 +1,250 @@
-import numpy
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import time
+import gc
+import os
+import csv
 from collections import namedtuple
+from IPython.display import FileLink
 
-def insertItems():
-    # Este es el ejemplo del fichero ks_4
-    Item = namedtuple("Item", ['index', 'value', 'weight'])
-    items = []
-    items.append(Item(0, 8, 4))
-    items.append(Item(1, 10, 5))
-    items.append(Item(2, 15, 8))
-    items.append(Item(3, 4, 3))
-    return items
+Item = namedtuple("Item", ['index', 'value', 'weight'])
+
+def submission_generation(filename, str_output):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for item in str_output:
+            writer.writerow(item)
+    return FileLink(filename)
+
+def check_solution(capacity, items, taken):
+    weight = 0
+    value = 0
+    for item in items:
+        if taken[item.index] == 1:
+            weight += item.weight
+            value += item.value
+    if weight > capacity:
+        print("solución incorrecta, se supera la capacidad de la mochila (capacity, weight):", capacity, weight)
+        return 0
+    return value
 
 class Node:
 
-    def __init__(self, value, capacity, bound):
-        self.root = None
-        self.left = None
+    def __init__(self, root, eValue, eWeight, bound):
+        self.father = root
         self.right = None
-        self.value = value
-        self.room = capacity
+        self.left = None
+        self.value = eValue
+        self.weight = eWeight
         self.estimate = bound
 
     def insert(self, newValue, newRoom, newEstimate):
-        newNode = Node(newValue, newRoom, newEstimate)
-        newNode.root = self
+        newNode = Node(self, newValue, newRoom, newEstimate)
         if newNode.estimate < self.estimate:
-            if self.right is None:
-                self.right = newNode
-            else:
-                self.right.insert(newNode.value, newNode.room, newNode.estimate)
+            self.right = newNode
         elif newNode.estimate == self.estimate:
-            if self.left is None:
-                self.left = newNode
-            else:
-                self.left.insert(newNode.value, newNode.room, newNode.estimate)  
+            self.left = newNode
 
-# Print tree
-    def PrintTree(self):
-        if self.left:
-            self.left.PrintTree()
-        print([self.value, self.room, self.estimate]),
-        if self.right:
-            self.right.PrintTree()
 
-if __name__ == "__main__":
-    #Inserta los items y crea el taken
-    items = insertItems()
-    taken = [0]*11
-    
-    #Calcula la informacion del nodo raiz y la crea: VALUE ROOM ESTIMATE
-    estimate = 0
-    for i in items:
-        estimate += i.value
-    act = Node(0, 11, estimate)
-    ant = None
-    res = None
-    root = act
-    
-    def searchChild(act, ant, pos):
-        if res == None:
-            for item in items:
-                if act.room - item.weight >= 0:
-                    act.insert(act.value + item.value, act.room - item.weight, act.estimate)
-                    ant = act
-                    act = ant.left
-                else:
-                    act.insert(act.value, act.room, act.estimate - item.value)
-                    ant = act
-                    act = ant.right
-            return ant, act, root
-        else:
-            if res.value < ant.estimate - items[pos].value:
-                ant.insert(ant.value, ant.room, ant.estimate - items[pos].value)
-                act = ant.right
-                ant = act.root
-            for i in range(pos + 1, len(items)):
-                if res.value < act.estimate:
-                    if act.room - items[i].weight >= 0:
-                        act.insert(act.value + items[i].value, act.room - items[i].weight, act.estimate)
-                        act = ant.left
-                        ant = act.root
-                    elif res.value < act.estimate - items[i].value:
-                        act.insert(act.value, act.room, act.estimate - items[i].value)
-                        act = ant.right
-                        ant = act.root
+def solve_it(input_data):
+    """"
+    The program first tries to run with Branch and Bound, if it is not posible
+    or takes a lot of time, it changes to a greedy algorithm
+    """
+
+    # parse the input
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    """"
+    Created a list to only add the items 
+    which are less or equal in weight than the total capacity of the knapsack
+    """
+    items = []
+    j = 1
+    for i in range(1, item_count + 1):
+        line = lines[i]
+        parts = line.split()
+        if int(parts[1]) <= capacity and int(parts[0]) > 0:
+            items.append(Item(j - 1, int(parts[0]), int(parts[1])))
+            j = j + 1
+
+    def values(it):
+        return it.value
+
+    def weights(it):
+        return it.weight
+
+    def valuesWeight(it):
+        return it.value / it.weight
+
+    item_count = len(items)
+    sortedItems = items.copy()
+    sortedItems.sort(reverse=True, key=valuesWeight)
+
+    """
+        Created two same lists as "items", one list is sorted by decreasing values, 
+        other by decreasing value/weight and other by increasing weights
+        Created to new knapsacks and taken list for the new lists created before
+
+        Greedy algorithm
+        1. Start from the first item of all three lists (they are iterated simultaneously)
+        2. If the weight of the item is lower or equal than the current capacity, the item is taken,
+        the weight is subtracted from the capacity of the respected knapsack and the value of the item taken 
+        is added to the current value of the knapsack (is done for each sorted list)
+        3. Once all of the respected 3 knapsacks cannot be filled by any more items
+           we choose the knapsack with the maximum value and delete the other lists
+        4. Return the profit and the respected taken list
+        """
+
+    def greedy():
+        itemsVW = items.copy()
+        itemsVW.sort(reverse=True, key=valuesWeight)
+        itemsW = items.copy()
+        itemsW.sort(key=weights)
+        items.sort(reverse=True, key=values)
+
+        valueGreedyV = valueGreedyVW = valueGreedyW = 0
+        weightV = weightVW = weightW = 0
+        takenGreedyV = [0] * len(items)
+        takenGreedyVW = [0] * len(items)
+        takenGreedyW = [0] * len(items)
+
+        def checkWeights(_takenGreedy, _weight, _item, _valueGreedy):
+            if _weight + _item.weight <= capacity:
+                _takenGreedy[_item.index] = 1
+                _valueGreedy += _item.value
+                _weight += _item.weight
+            return _takenGreedy, _weight, _valueGreedy
+
+        for itemV, itemVW, itemW in zip(items, itemsVW, itemsW):
+            if weightV == capacity and weightVW == capacity and weightW == capacity:
                 break
-            return ant, act, root
-    
-    def reverseTree(ant, act, res):
-        i = len(items) - 1
-        while (ant != root):
-            if ant.right == None:
-                ant1, act1, root1 = searchChild(act, ant, i)
-                if act1.value > res.value: res = act1
-                #i = len(items) - 1
-            ant = ant.root
-            act = act.root
-            i -= 1
-            #if i == 0:
-            #    if root.right == None:
-            #        if res.value > root.estimate - items[i].value: break
-            #        i = len(items) - 1
-        return ant, act
+            takenGreedyV, weightV, valueGreedyV = checkWeights(takenGreedyV, weightV, itemV, valueGreedyV)
+            takenGreedyVW, weightVW, valueGreedyVW = checkWeights(takenGreedyVW, weightVW, itemVW, valueGreedyVW)
+            takenGreedyW, weightW, valueGreedyW = checkWeights(takenGreedyW, weightW, itemW, valueGreedyW)
 
-    ant, act, root = searchChild(act, ant, 0)
-    res = act
-    
-    a, s = reverseTree(ant, act, res)
+        del (itemsW)
+        del (itemsVW)
+        gc.collect()
+        valueGreedy = max(valueGreedyV, valueGreedyVW, valueGreedyW)
 
-    print(root.PrintTree())
-    print(res.value, res.estimate)
+        if valueGreedy == valueGreedyV:
+            del (takenGreedyVW)
+            del (takenGreedyW)
+            gc.collect()
+            return valueGreedy, takenGreedyV
+        elif valueGreedy == valueGreedyVW:
+            del (takenGreedyV)
+            del (takenGreedyW)
+            gc.collect()
+            return valueGreedy, takenGreedyVW
+        else:
+            del (takenGreedyV)
+            del (takenGreedyVW)
+            gc.collect()
+            return valueGreedy, takenGreedyW
+
+        """
+            Branch and Bound algorithm
+            1. Calculte factionary bound and create a dummy node with value = 0, room = capacity and bound calculated
+            2. While the tree has not reached to a leaf node, insert the items than not exceed the capacity (like a greedy)
+            3. Store the leaf node and start backtracking comparing the value of the leaf node with the estimate
+            4. If a leaf node with a better node value is found, it is replaced by the stored leaf node in 3.
+            5. Explore the right side of the node for a better solution
+            Exceptions: if the algorithm takes more than 30 seconds or any exception ocurrs
+            the algorithm is changed to a greedy
+            """
+
+    def brABound():
+        takenB = [0] * len(sortedItems)
+        estimate = 0
+        weights = 0
+
+        for x in sortedItems:
+            if weights + x.weight <= capacity:
+                estimate += x.value
+                weights += x.weight
+            else:
+                res = capacity - weights
+                estimate += (res * x.value) / x.weight
+                break
+        root = Node(None, 0, capacity, estimate)
+        act = root
+        i = 0
+        result = Node(None, 0, 0, 0)
+
+        start = time.time()
+        while root.right is None or act.father is not None:
+            if act.left is None and act.weight - sortedItems[i].weight >= 0:
+                act.insert(act.value + sortedItems[i].value, act.weight - sortedItems[i].weight, act.estimate)
+                ant = act
+                act = act.left
+            else:
+                act.insert(act.value, act.weight, act.estimate - sortedItems[i].value)
+                ant = act
+                act = ant.right
+            i += 1
+            end = time.time()
+
+            if end - start >= 15:
+                root.right = None
+                root.left = None
+                gc.collect()
+                return greedy()
+
+            if act.value > result.value:
+                result = act
+
+            if i == len(items) or ant.estimate > result.estimate:
+                i -= 1
+
+                while ant.right is not None and ant.father is not None:
+                    act = ant
+                    ant = act.father
+                    i -= 1
+                act = act.father
+
+        def searchNode(resultado, anterior):
+            valor = resultado.value - anterior.value
+            peso = anterior.weight - resultado.weight
+
+            if valor != 0 and peso != 0:
+                for it in sortedItems:
+                    if peso == it.weight and valor == it.value:
+                        return it.index
+
+        # Taken of BAB
+        res = result.value
+        aux = result.father
+        while aux is not None:
+            if result is not None and result.value > aux.value:
+                ind = searchNode(result, aux)
+                takenB[ind] = 1
+            result = result.father
+            aux = aux.father
+        return res, takenB
+
+    value, taken = brABound()
+
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data, check_solution(capacity, sortedItems, taken)
+
+if __name__ == '__main__':
+    for dirname, _, filenames in os.walk('data'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+    str_output = [["Filename", "Max_value"]]
+    for dirname, _, filenames in os.walk('data'):
+        for filename in filenames:
+            full_name = dirname + '/' + filename
+            with open(full_name, 'r') as input_data_file:
+                input_data = input_data_file.read()
+                output, value = solve_it(input_data)
+                str_output.append([filename, str(value)])
+    submission_generation('greedy&BB.csv', str_output)
